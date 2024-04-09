721554fe85400eb0433e5d92f1363332c25d2a72
Vinh Nguyen <vinhnguyentan99@gmail.com>
2024-04-09 13:37:03 +0700

    feat: demo overwrite for textbox

--- src/shapes/c-textbox.ts
+++ src/shapes/c-textbox.ts
@@ -1,108 +1,44 @@
-// import * as fabric from 'fabric';
-// import { cloneDeep, forEach } from 'lodash';
-// import { TClassProperties } from '../type';
+import * as fabric from 'fabric';
+import { TClassProperties } from '../type';
 
-// fabric.config.disableStyleCopyPaste = true;
+export const textBoxDefaultValues: Partial<TClassProperties<CorjlTextbox>> = {
+  minWidth: 20,
+  dynamicMinWidth: 2,
+  lockScalingFlip: true,
+  noScaleCache: false,
+  _wordJoiners: /[ \t\r]/,
+  splitByGrapheme: false,
+}
+export class CorjlTextbox extends fabric.Textbox {
+  static stateProperties: string[] = fabric.Textbox.stateProperties.concat(
+    'originalText',
+    'uppercase',
+    'curved',
+    'outerGlow',
+    'protection',
+    'freeze',
+    'wrapText',
+    'autoWrapText',
+  );
+  static cacheProperties = fabric.Textbox.cacheProperties.concat(
+    'originalText',
+    'uppercase',
+    'curved',
+    'outerGlow',
+    'protection',
+    'freeze',
+    'wrapText',
+    'autoWrapText',
+  );
 
-// const ANTI_WRAP_MARGIN = 1.2;
-// export const textBoxDefaultValues: Partial<TClassProperties<CorjlTextbox>> = {
-//   ITALIC_ANGLE: 15,
-//   paintFirst: 'stroke',
-//   strokeLineJoin: 'round',
-//   _reNewline: new RegExp(/\r?\n/),
-//   imageRenderSize: {
-//     width: 0,
-//     height: 0,
-//   },
-//   textLineMaps: {},
-//   originXOffset: {
-//     left: -0.5,
-//     center: 0,
-//     right: 0.5
-//   },
-//   originYOffset: {
-//     top: -0.5,
-//     center: 0,
-//     bottom: 0.5
-//   },
-//   _styleProperties: [
-//     'stroke',
-//     'strokeWidth',
-//     'fill',
-//     'gradient',
-//     'fontFamily',
-//     'fontSize',
-//     'fontWeight',
-//     'fontStyle',
-//     'underline',
-//     'overline',
-//     'linethrough',
-//     'deltaY',
-//     'textBackgroundColor',
-//   ],
-//   cacheDynamicProperties: [
-//     'fontFamily',
-//     'fontSize',
-//     'lineHeight',
-//     'originalText',
-//     'uppercase',
-//     'charSpacing',
-//     'styles',
-//     'width',
-//     'wrapText',
-//     'autoWrapText',
-//   ],
-//   _dimensionAffectingProps: [
-//     'fontSize',
-//     'fontWeight',
-//     'fontFamily',
-//     'fontStyle',
-//     'lineHeight',
-//     'text',
-//     'originalText',
-//     'charSpacing',
-//     'textAlign',
-//     'styles',
-//     'path',
-//     'pathStartOffset',
-//     'pathSide',
-//     'pathAlign',
-//     'bulletStyles',
-//     'uppercase',
-//     'protection'
-//   ],
-//   objectCaching: false
-// }
-// export class CorjlTextbox extends fabric.Textbox {
-//   static stateProperties: string[] = fabric.Textbox.stateProperties.concat(
-//     'originalText',
-//     'uppercase',
-//     'curved',
-//     'outerGlow',
-//     'protection',
-//     'freeze',
-//     'wrapText',
-//     'autoWrapText',
-//   );
-//   static cacheProperties = fabric.Textbox.cacheProperties.concat(
-//     'originalText',
-//     'uppercase',
-//     'curved',
-//     'outerGlow',
-//     'protection',
-//     'freeze',
-//     'wrapText',
-//     'autoWrapText',
-//   );
+  static ownDefaults: Record<string, any> = textBoxDefaultValues;
 
-//   static ownDefaults: Record<string, any> = textBoxDefaultValues;
+  static getDefaults(): { [x: string]: any; } {
+    return { ...super.getDefaults(), ...CorjlTextbox.ownDefaults };
+  }
 
-//   static getDefaults(): { [x: string]: any; } {
-//     return { ...super.getDefaults(), ...CorjlTextbox.ownDefaults };
-//   }
-
-//   constructor(text: string, options?: any) {
-//     super(text, options);
-//     console.log(text, 'text...');
-//   }
-// }
+  constructor(text: string, options?: any) {
+    super(text, options);
+    console.log(text, 'text...');
+  }
+}
