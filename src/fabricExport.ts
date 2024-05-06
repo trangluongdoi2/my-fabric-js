b36e5c5c984fbac09e2278af9871811e36763278
Vinh Nguyen <vinhnguyentan99@gmail.com>
2024-05-06 16:07:09 +0700

    feat: update render init text

--- src/fabricExport.ts
+++ src/fabricExport.ts
@@ -1,3 +1,6 @@
+// import './shapes/text/c-text';
+import './shapes/text/c-itext';
+
 export * from 'fabric';
 
 export * from './c-canvas';
--- src/shapes/c-textbox.ts
+++ src/shapes/c-textbox.ts
@@ -1,3 +1,4 @@
+import { TPointerEvent } from 'EventTypeDefs';
 import * as fabric from 'fabric';
 import { TClassProperties } from '../type';
 
@@ -33,17 +34,86 @@ export class MyTextbox extends fabric.Textbox {
     super(text, options);
   }
 
+  render(ctx: CanvasRenderingContext2D) {
+    super.render(ctx);
+    ctx.save();
+    this._setOpacity(ctx);
+    ctx.restore();
+  }
+
+  isEdittingEmptyText() {
+    return !this.originalText && this.isEditing;
+  }
+
+  _setOpacity(ctx: CanvasRenderingContext2D) {
+    if (!this.originalText && this.text?.toUpperCase() === this.placeholder?.toUpperCase()) {
+      ctx.globalAlpha *= 0.5;
+      return;
+    }
+    super._setOpacity(ctx);
+  }
+
   // @ts-ignore
   get text() {
     let text = this.originalText || this.placeholder || '';
-    // if (this.isEdittingEmptyText()) {
-    //   text = '';
-    // }
+    if (this.isEdittingEmptyText()) {
+      text = '';
+    }
     return text;
   }
 
   set text(value: string) {
     const text = value || '';
     this.originalText = text;
+  }
+
+  onInput(event: any) {
+    if (!this.hiddenTextarea) {
+      return;
+    }
+    super.onInput(event);
+    this.originalText = this.hiddenTextarea.value;
+  }
+
+  enterEditing(e?: TPointerEvent): any {
+    if (this.isEditing || !this.editable) {
+      return;
+    }
+
+    this.isEditing = true;
+    this.initHiddenTextarea();
+    if (this.lineHeight === 0 && this.hiddenTextarea) {
+      this.hiddenTextarea.style.top = '0';
+      this.hiddenTextarea.style.left = '0';
+    }
+
+    if (this.hiddenTextarea) {
+      this.hiddenTextarea.focus();
+      this.hiddenTextarea.value = this.originalText || '';
+      this.hiddenTextarea.style.paddingTop = '0';
+      this._updateTextarea();
+      this._saveEditingProps();
+      this._setEditingProps();
+      (<any>this)._textBeforeEdit = this.text;
+
+      (<any>this)._tick();
+      this.fire('editing:entered');
+      this._fireSelectionChanged();
+      if (!this.canvas) {
+        return this;
+      }
+      this.canvas.fire('text:editing:entered', { target: this, e } as any);
+      this.canvas.requestRenderAll();
+
+    }
+    return this;
+  }
+
+  // @ts-ignore
+  toObject(propertiesToInclude?: string[]) {
+    // @ts-ignore
+    return super.toObject([
+      'originalText',
+    ].concat(propertiesToInclude || []));
   }
 }
